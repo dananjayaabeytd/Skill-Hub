f4f84e0ae24192c05d14a3ed0ad337f7009aa5df
Dananjaya Abenayaka <dananjayaabeytd@gmail.com>
2025-03-16 09:45:14 +0530

    feat(user): add findByEmail to user repository

--- backend/src/main/java/com/paf/skillhub/repositories/UserRepository.java
+++ backend/src/main/java/com/paf/skillhub/repositories/UserRepository.java
@@ -13,4 +13,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
   Boolean existsByUserName(String username);
 
   Boolean existsByEmail(String email);
+
+  Optional<User> findByEmail(String email);
 }
