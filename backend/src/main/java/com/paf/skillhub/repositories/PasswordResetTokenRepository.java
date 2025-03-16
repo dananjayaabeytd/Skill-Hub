d60edc0629a3c473ffad88ee04842da6346b2f3b
Dananjaya Abenayaka <dananjayaabeytd@gmail.com>
2025-03-16 09:44:56 +0530

    feat(user): add password reset token repository

---
+++ backend/src/main/java/com/paf/skillhub/repositories/PasswordResetTokenRepository.java
@@ -0,0 +1,11 @@
+package com.paf.skillhub.repositories;
+
+
+import com.paf.skillhub.models.PasswordResetToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
+
+}
