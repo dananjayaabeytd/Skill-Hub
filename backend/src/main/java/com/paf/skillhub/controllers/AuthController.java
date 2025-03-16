a76f0d5b1f3b604af6f3dd8ddb637a48a905ea83
Dananjaya Abenayaka <dananjayaabeytd@gmail.com>
2025-03-16 09:22:13 +0530

    feat(user): update auth controller

--- backend/src/main/java/com/paf/skillhub/controllers/AuthController.java
+++ backend/src/main/java/com/paf/skillhub/controllers/AuthController.java
@@ -1,8 +1,18 @@
 package com.paf.skillhub.controllers;
 
+import com.paf.skillhub.models.AppRole;
+import com.paf.skillhub.models.Role;
+import com.paf.skillhub.models.User;
+import com.paf.skillhub.repositories.RoleRepository;
+import com.paf.skillhub.repositories.UserRepository;
 import com.paf.skillhub.security.jwt.JwtUtils;
 import com.paf.skillhub.security.request.LoginRequest;
+import com.paf.skillhub.security.request.SignupRequest;
 import com.paf.skillhub.security.response.LoginResponse;
+import com.paf.skillhub.security.response.MessageResponse;
+import jakarta.validation.Valid;
+import java.time.LocalDate;
+import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +22,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,6 +42,15 @@ public class AuthController {
 
   @Autowired
   AuthenticationManager authenticationManager;
+
+  @Autowired
+  UserRepository userRepository;
+
+  @Autowired
+  RoleRepository roleRepository;
+
+  @Autowired
+  PasswordEncoder encoder;
 
   @PostMapping("/public/signin")
   public ResponseEntity<?> authenticateUser(@RequestBody LoginRequest loginRequest) {
@@ -65,5 +85,53 @@ public class AuthController {
 
     // Return the response entity with the JWT token included in the response body
     return ResponseEntity.ok(response);
+  }
+
+  @PostMapping("/public/signup")
+  public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
+    if (userRepository.existsByUserName(signUpRequest.getUsername())) {
+      return ResponseEntity.badRequest()
+          .body(new MessageResponse("Error: Username is already taken!"));
+    }
+
+    if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+      return ResponseEntity.badRequest()
+          .body(new MessageResponse("Error: Email is already in use!"));
+    }
+
+    // Create new user's account
+    User user = new User(signUpRequest.getUsername(),
+        signUpRequest.getEmail(),
+        encoder.encode(signUpRequest.getPassword()));
+
+    Set<String> strRoles = signUpRequest.getRole();
+    Role role;
+
+    if (strRoles == null || strRoles.isEmpty()) {
+      role = roleRepository.findByRoleName(AppRole.ROLE_USER)
+          .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+    } else {
+      String roleStr = strRoles.iterator().next();
+      if (roleStr.equals("admin")) {
+        role = roleRepository.findByRoleName(AppRole.ROLE_ADMIN)
+            .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+      } else {
+        role = roleRepository.findByRoleName(AppRole.ROLE_USER)
+            .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+      }
+
+      user.setAccountNonLocked(true);
+      user.setAccountNonExpired(true);
+      user.setCredentialsNonExpired(true);
+      user.setEnabled(true);
+      user.setCredentialsExpiryDate(LocalDate.now().plusYears(1));
+      user.setAccountExpiryDate(LocalDate.now().plusYears(1));
+      user.setTwoFactorEnabled(false);
+      user.setSignUpMethod("email");
+    }
+    user.setRole(role);
+    userRepository.save(user);
+
+    return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
   }
 }
