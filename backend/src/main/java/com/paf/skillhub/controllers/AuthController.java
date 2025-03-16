7dc7dd595def33e1d110459756062c14f5e27103
Dananjaya Abenayaka <dananjayaabeytd@gmail.com>
2025-03-16 09:46:02 +0530

    feat(user): update auth controller with forgotPassword endpoint

--- backend/src/main/java/com/paf/skillhub/controllers/AuthController.java
+++ backend/src/main/java/com/paf/skillhub/controllers/AuthController.java
@@ -30,6 +30,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -170,5 +171,17 @@ public class AuthController {
   @GetMapping("/username")
   public String currentUserName(@AuthenticationPrincipal UserDetails userDetails) {
     return (userDetails != null) ? userDetails.getUsername() : "";
+  }
+
+  @PostMapping("/public/forgot-password")
+  public ResponseEntity<?> forgotPassword(@RequestParam String email){
+    try{
+      userService.generatePasswordResetToken(email);
+      return ResponseEntity.ok(new MessageResponse("Password reset email sent!"));
+    } catch (Exception e){
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+          .body(new MessageResponse("Error sending password reset email"));
+    }
+
   }
 }
