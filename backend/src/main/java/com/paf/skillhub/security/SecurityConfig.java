627c3d39ea5c19aa6fc4cc4fe28f9b8ef0aeec1f
Dananjaya Abenayaka <dananjayaabeytd@gmail.com>
2025-03-16 08:58:58 +0530

    feat(user): update security config to permit csrf urls

--- backend/src/main/java/com/paf/skillhub/security/SecurityConfig.java
+++ backend/src/main/java/com/paf/skillhub/security/SecurityConfig.java
@@ -11,10 +11,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -24,11 +24,16 @@ public class SecurityConfig {
 
   @Bean
   SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+
+    http.csrf(csrf ->
+        csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()));
+
+    //http.csrf(AbstractHttpConfigurer::disable);
     http.authorizeHttpRequests((requests)
         -> requests
         .requestMatchers("/api/admin/**").hasRole("ADMIN")
+        .requestMatchers("/api/csrf-token").permitAll()
         .anyRequest().authenticated());
-    http.csrf(AbstractHttpConfigurer::disable);
     //http.formLogin(withDefaults());
     http.httpBasic(withDefaults());
     return http.build();
