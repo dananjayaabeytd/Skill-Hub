d4087829777854be3ba33f10ab0fd279c28a21ac
Dananjaya Abenayaka <dananjayaabeytd@gmail.com>
2025-03-16 08:46:53 +0530

    feat(user): add user service interface

---
+++ backend/src/main/java/com/paf/skillhub/services/UserService.java
@@ -0,0 +1,14 @@
+package com.paf.skillhub.services;
+
+import com.paf.skillhub.dtos.UserDTO;
+import com.paf.skillhub.models.User;
+import java.util.List;
+
+public interface UserService {
+
+  void updateUserRole(Long userId, String roleName);
+
+  List<User> getAllUsers();
+
+  UserDTO getUserById(Long id);
+}
