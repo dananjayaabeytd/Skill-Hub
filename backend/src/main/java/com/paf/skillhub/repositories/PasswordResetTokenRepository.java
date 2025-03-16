4026c3f47d1c4f56c39f5c067249891d0e02a518
Dananjaya Abenayaka <dananjayaabeytd@gmail.com>
2025-03-16 09:51:45 +0530

    feat(user): add findByToken method to password reset repository

--- backend/src/main/java/com/paf/skillhub/repositories/PasswordResetTokenRepository.java
+++ backend/src/main/java/com/paf/skillhub/repositories/PasswordResetTokenRepository.java
@@ -2,10 +2,13 @@ package com.paf.skillhub.repositories;
 
 
 import com.paf.skillhub.models.PasswordResetToken;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
+
+  Optional<PasswordResetToken> findByToken(String token);
 
 }
