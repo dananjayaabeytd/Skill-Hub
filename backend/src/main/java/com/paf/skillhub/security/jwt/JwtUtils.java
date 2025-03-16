9334abe0eb0e767145437f0fa04335e80efa6cda
Dananjaya Abenayaka <dananjayaabeytd@gmail.com>
2025-03-16 09:57:54 +0530

    feat(cors): add roles for jwt

--- backend/src/main/java/com/paf/skillhub/security/jwt/JwtUtils.java
+++ backend/src/main/java/com/paf/skillhub/security/jwt/JwtUtils.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.http.HttpServletRequest;
+import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -30,17 +31,23 @@ public class JwtUtils {
     String bearerToken = request.getHeader("Authorization");
     logger.debug("Authorization Header: {}", bearerToken);
 
-    if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+    if (bearerToken != null && bearerToken.startsWith("Bearer "))
+    {
       return bearerToken.substring(7); // Remove Bearer prefix
     }
     return null;
   }
 
-  public String generateTokenFromUsername(UserDetails userDetails) {
+  public String generateTokenFromUsername(UserDetails userDetails)
+  {
     String username = userDetails.getUsername();
+    String roles = userDetails.getAuthorities().stream()
+        .map(authority -> authority.getAuthority())
+        .collect(Collectors.joining(","));
 
     return Jwts.builder()
         .subject(username)
+        .claim("roles", roles)
         .issuedAt(new Date())
         .expiration(new Date((new Date()).getTime() + jwtExpirationMs))
         .signWith(key())
@@ -48,30 +55,38 @@ public class JwtUtils {
 
   }
 
-  public String getUserNameFromJwtToken(String token) {
+  public String getUserNameFromJwtToken(String token)
+  {
     return Jwts.parser()
         .verifyWith((SecretKey) key())
         .build().parseSignedClaims(token)
         .getPayload().getSubject();
   }
 
-  private Key key() {
+  private Key key()
+  {
     return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
   }
 
-  public boolean validateJwtToken(String authToken) {
-    try {
+  public boolean validateJwtToken(String authToken)
+  {
+    try
+    {
       System.out.println("Validate");
       Jwts.parser().verifyWith((SecretKey) key())
           .build().parseSignedClaims(authToken);
       return true;
-    } catch (MalformedJwtException e) {
+    } catch (MalformedJwtException e)
+    {
       logger.error("Invalid JWT token: {}", e.getMessage());
-    } catch (ExpiredJwtException e) {
+    } catch (ExpiredJwtException e)
+    {
       logger.error("JWT token is expired: {}", e.getMessage());
-    } catch (UnsupportedJwtException e) {
+    } catch (UnsupportedJwtException e)
+    {
       logger.error("JWT token is unsupported: {}", e.getMessage());
-    } catch (IllegalArgumentException e) {
+    } catch (IllegalArgumentException e)
+    {
       logger.error("JWT claims string is empty: {}", e.getMessage());
     }
     return false;
