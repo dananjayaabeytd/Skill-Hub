0f9d9d86d61d5596fc0f4a7a88f68311e4a1b93f
Dananjaya Abenayaka <dananjayaabeytd@gmail.com>
2025-03-27 20:04:16 +0530

    feat(skill): email service util

--- backend/src/main/java/com/paf/skillhub/utils/EmailService.java
+++ backend/src/main/java/com/paf/skillhub/utils/EmailService.java
@@ -1,6 +1,5 @@
 package com.paf.skillhub.utils;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -8,10 +7,10 @@ import org.springframework.stereotype.Service;
 @Service
 public class EmailService {
 
-  @Autowired
   private JavaMailSender mailSender;
 
-  public void sendPasswordResetEmail(String to, String resetUrl){
+  public void sendPasswordResetEmail(String to, String resetUrl)
+  {
     SimpleMailMessage message = new SimpleMailMessage();
     message.setTo(to);
     message.setSubject("Password Reset Request");
