47cb4efdddb0078d05898994a410e61575801574
Dananjaya Abenayaka <dananjayaabeytd@gmail.com>
2025-03-28 13:31:31 +0530

    feat(follower): add  follow feature

---
+++ backend/src/main/java/com/paf/skillhub/Follow/repositories/FollowerRepository.java
@@ -0,0 +1,28 @@
+package com.paf.skillhub.Follow.repositories;
+
+
+import com.paf.skillhub.Follow.models.Follower;
+import com.paf.skillhub.User.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface FollowerRepository extends JpaRepository<Follower, Long> {
+
+  Optional<Follower> findByUserAndFollowerUser(User user, User followerUser);
+
+  long countByUser(User user);
+
+  List<Follower> findByUser(User user);
+
+  List<Follower> findByFollowerUser(User followerUser);
+
+  @Query("SELECT COUNT(f) > 0 FROM Follower f WHERE f.user.userId = :userId AND f.followerUser.userId = :followerUserId")
+  boolean existsByUserIdAndFollowerUserId(Long userId, Long followerUserId);
+
+  long countByFollowerUser(User followerUser);
+}
