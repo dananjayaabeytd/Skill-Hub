98eef7a914ff4011c155d374d42539bc33fe4e2f
Dananjaya Abenayaka <dananjayaabeytd@gmail.com>
2025-04-26 23:27:08 +0530

    frontend - fix(user): routes

--- frontend/src/layout/root.layout.jsx
+++ frontend/src/layout/root.layout.jsx
@@ -1,11 +1,14 @@
 import { Outlet } from 'react-router-dom';
 import { MenuBar } from '../components/shared/Navbar';
 import { FooterComponent } from '../components/shared/Footer';
+import { Toaster } from 'react-hot-toast';
+import { ContextProvider } from '../store/ContextApi';
 
 function RootLayout() {
   return (
     <>
       <MenuBar />
+      {/* <Toaster position='bottom-center' reverseOrder={false} /> */}
       <Outlet />
       <FooterComponent />
     </>
