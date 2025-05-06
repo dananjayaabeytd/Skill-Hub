a196fba08b6ce39b500d5d22b52bd963e2a08add
Dananjaya Abenayaka <dananjayaabeytd@gmail.com>
2025-05-06 20:13:44 +0530

    frontend - feat(chatbot): route

--- frontend/src/layout/root.layout.jsx
+++ frontend/src/layout/root.layout.jsx
@@ -3,12 +3,14 @@ import { MenuBar } from '../components/shared/Navbar';
 import { FooterComponent } from '../components/shared/Footer';
 import { Toaster } from 'react-hot-toast';
 import { ContextProvider } from '../store/ContextApi';
+import ChatbotBubble from '../components/ChatbotBubble';
 
 function RootLayout() {
   return (
     <>
       <MenuBar />
       {/* <Toaster position='bottom-center' reverseOrder={false} /> */}
+      <ChatbotBubble />
       <Outlet />
       <FooterComponent />
     </>
